c4bcf76d332449c1328dad9c280926b0a4fa015d
Astrid Hecht <silentaviary@gmail.com>
2022-12-12 15:17:47 -0700

    refactor: refactor to clean up controller methods and styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,10 @@
 require_relative '../facades/movie_facade'
 
 class UsersController < ApplicationController
-  def new
-  end
-
   def create
-    fix_raw_params
-    new_user = User.new(user_params)
-    return password_error(new_user.errors) unless password_match?
+    return unless password_confirmation?
 
-    if new_user.save
+    if (new_user = User.new(user_params)).save
       flash[:success] = "Welcome, #{new_user.email}!"
       redirect_to "/users/#{new_user.id}"
     else
@@ -23,9 +18,7 @@ class UsersController < ApplicationController
   end
 
   def login_user
-    fix_raw_params
-    user = User.find_by(email: params[:email])
-    if valid_login_params? && user && user.authenticate(params[:password])
+    if valid_login?(user = email_find(params[:email]))
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}!"
       redirect_to user_path(user)
@@ -37,28 +30,31 @@ class UsersController < ApplicationController
 
   private
 
-  def password_match?
-    params[:user][:password] == params[:user][:password_confirmation]
-  end
-
-  def fix_raw_params
-    params[:user][:email] = params[:user][:email].downcase if params[:user]
-    params[:email] = params[:email].downcase unless params[:user]
-  end
-
   def user_params
+    params[:user][:email].downcase!
     params.require(:user).permit(:name, :email, :password)
   end
 
-  def valid_login_params?
-    params[:email].present? && params[:password].present?
+  def password_confirmation?
+    return true if params[:user][:password] == params[:user][:password_confirmation]
+
+    redirect_to '/register'
+    flash[:alert] = 'Error: Passwords must match'
+    false
   end
 
-  def password_error(errors)
-    redirect_to '/register'
-    flash[:alert] = "Error: Passwords must match #{", #{error_message(errors)}" if errors}"
+  def email_find(email)
+    User.find_by(email: email.downcase)
   end
-  
+
+  def valid_login?(user)
+    return false unless params[:email].present? && params[:password].present?
+    return false unless user
+    return false unless user.authenticate(params[:password])
+
+    true
+  end
+
   def new_user_error(errors)
     redirect_to '/register'
     flash[:alert] = "Error: #{error_message(errors)}"
